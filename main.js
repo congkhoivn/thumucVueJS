fe7935892c343686325087e4f389035e00d5afaf
congkhoivn <congkhoi6109@gmail.com>
2024-08-12 16:58:12 +0700

    co_quan

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
+var vm = new Vue({
+  el:'#map',
+  data:{
+    title:'Điện thoại Samsung',
+    dt1:'Nokia',
+    dt2:'Apple'
+  } 
+});
+console.log(vm);
 
-  var vm = new Vue({
-    el:'#map',
-    data:{
-        title: 'dien thoai Isphone'
+setTimeout(() =>{
+  vm.title='Điện thoại Xiaomi'
+  vm.dt1='Điện thoại Oppo'
+  vm.dt2='Điện thoại Iphone'
+},5000
+);
+
+var click= new Vue({
+  el:'.dulieu',
+  data:{
+    count:1,
+    clientX: 0,
+    clientY: 0
+  },
+  methods:{
+    handleClick(number, event){
+      console.log('click',event.target);
+      this.count += number;
     },
-  });
-  console.log(vm);
-
-  setTimeout (() =>{
-    vm.title='Điện thoại SamSung';
-  },4000
-  );
+    handleMouseMove(e){
+       console.log(e);
+      this.clientX = e.clientX;
+      this.clientY = e.clientY;
+    }
+  }
+}
+);
